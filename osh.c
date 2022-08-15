fe67a712e19a23ac60b9147bf5f54d2cc51896cc
Anil G R <anil@foradian.com>
2022-08-15 14:40:50 +0530

    feat: execute input commands.

--- osh.c
+++ osh.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "osh.h"
@@ -7,11 +7,8 @@
 int main(void)
 {
     string args[MAX_LINE / 2 + 1]; /* command line arguments */
-    int should_run = 1; 
-    int argsCount;          /* flag to determine when to exit program */
-
-    printf("osh>");
-    fflush(stdout);
+    int should_run = 1;
+    int argsCount; /* flag to determine when to exit program */
     /**
      * After reading user input, the steps are:
      * (1) fork a child process using fork()
@@ -21,13 +18,30 @@ int main(void)
     // get user input
     char *command = malloc(MAX_LINE * sizeof(char));
 
-    scanf(" %[^\n]", command);
+    while (should_run)
+    {
+        //show prompt
+        printf("osh>");
+        fflush(stdout);
 
-    //parse user input
-    argsCount = parseCommand(command, args);
+        // read input
+        scanf(" %[^\n]", command);
+
+        // parse user input
+        argsCount = parseCommand(command, args);
+
+        pid_t pid;
+        pid = fork();
+
+        if (pid == 0)
+        {
+            execvp(args[0], args);
+        }
+        else if (pid > 0)
+        {
+            wait(NULL);
+        }
+    }
 
     return 0;
 }
-
-
-
--- parser.c
+++ parser.c
@@ -6,7 +6,6 @@
 int parseCommand(char *command, string args[]) {
     int argsIndex = 0;
 
-    printf("%s", command);
     args[argsIndex] = malloc(MAX_ARG_SIZE * sizeof(char));
     int k = 0;
     for (int i = 0; i < strlen(command); i++) {
@@ -22,8 +21,8 @@ int parseCommand(char *command, string args[]) {
         }
     }
     int argsCount = argsIndex + 1;
-    for (int j = 0; j < argsCount; j++) {
-        printf("\n%s\n", args[j]);
+    for (int j = argsCount; j < MAX_LINE / 2 + 1; j++) {
+        args[j] = NULL;
     }
 
     return argsCount;
